08cede248b8950be4cf55f5b3da9883a6fc2c7ab
mogl1que <mogl1quefn@gmail.com>
2025-09-18 19:49:34 +0300

    Update java.java

--- topic/java.java
+++ topic/java.java
@@ -1,1 +1,15 @@
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        // создаём список, используемый как стек
+        ArrayList<Character> stack = new ArrayList<>();
+
+        stack.add('a');  // добавляем 'a' в стек
+        stack.add('b');  // добавляем 'b' в стек
+        stack.add('c');  // добавляем 'c' в стек
+
+        // выводим стек
+        System.out.println(stack);
+    }
+}
